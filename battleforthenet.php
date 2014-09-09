e465c90c011b46616c2bfcf1f3f0cfc80b786007
Josh Shashaty <josh@joshshashaty.com>
2014-09-09 12:04:13 -0400

    updated readme and battle images

--- battleforthenet.php
+++ battleforthenet.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Battle For The Net
 Description: We're in the battle for the net.  This plugin will display a linked image to BattleForTheNet.com to help spread the word on Net Neutrality
-Plugin URI: http://#
+Plugin URI: http://thewpvalet.com/
 Author: The WP Valet
 Author URI: http://thewpvalet.com/
 Version: 1.0
@@ -29,26 +29,35 @@ License: GPL2
 
 
 /**
- * Set default image size
+ * Set default battle image
  */
-define( 'BFTN_DEFAULT_SIZE', '500x300' );
+define( 'BFTN_PLUGIN_URI', plugin_dir_url(__FILE__) );
+define( 'BFTN_DEFAULT_BATTLE_IMAGE', BFTN_PLUGIN_URI . '/images/battle3.png' );
 
 
 
 /**
  * Outputs image and link to battleforthenet.com
- * @param  string 	$size 	size of image
- * @return string       	image/link output
+ * @param  string 	$battle_image 	id of battle image
+ * @return string       			image/link output
  */
-function battle_for_the_net( $size, $custom_image = null ){
+function battleforthenet_output( $battle_image_id, $custom_image = null ){
 
-	switch ( $size ) {
-		case '150x150':
-			$image_url = 'http://placehold.it/150x150';
+	switch ( $battle_image_id ) {
+		case '1':
+			$image_url = BFTN_PLUGIN_URI . '/images/battle1.png';
+			break;
+
+		case '2':
+			$image_url = BFTN_PLUGIN_URI . '/images/battle2.png';
+			break;
+
+		case '3':
+			$image_url = BFTN_PLUGIN_URI . '/images/battle3.png';
 			break;
 		
 		default:
-			$image_url = 'http://placehold.it/' . BFTN_DEFAULT_SIZE;
+			$image_url = BFTN_DEFAULT_BATTLE_IMAGE;
 			break;
 	}
 
@@ -61,14 +70,15 @@ function battle_for_the_net( $size, $custom_image = null ){
 }
 
 
+
 /**
  * Shortcode
  */
 add_shortcode( 'battleforthenet', 'bftn_shortcode_output' );
 
 function bftn_shortcode_output( $atts = null ){
-	extract( shortcode_atts( array( 'size' => BFTN_DEFAULT_SIZE, 'custom_image' => null ), $atts ) );
-	return battle_for_the_net( $size, $custom_image );
+	extract( shortcode_atts( array( 'battle_image' => BFTN_DEFAULT_BATTLE_IMAGE, 'custom_image' => null ), $atts ) );
+	return battleforthenet_output( $battle_image, $custom_image );
 }
 
 
@@ -102,7 +112,7 @@ class BFTN_Widget extends WP_Widget {
         echo $before_widget;
         echo $before_title;
         echo $instance['title']; // Can set this with a widget option, or omit altogether
-        echo battle_for_the_net( $instance['size'], $instance['custom_image'] );
+        echo battleforthenet_output( $instance['battle_image'], $instance['custom_image'] );
         echo $after_title;
     	echo $after_widget;
     }
@@ -112,7 +122,7 @@ class BFTN_Widget extends WP_Widget {
      */
     function update( $new_instance, $old_instance ) {
         $updated_instance['title'] = esc_html( $new_instance['title'] );
-        $updated_instance['size'] = esc_html( trim( $new_instance['size'] ) );
+        $updated_instance['battle_image'] = esc_html( trim( $new_instance['battle_image'] ) );
         $updated_instance['custom_image'] = esc_url( $new_instance['custom_image'] );
         return $updated_instance;
     }
@@ -121,7 +131,7 @@ class BFTN_Widget extends WP_Widget {
      * Widget form
      */
     function form( $instance ) {
-        $instance = wp_parse_args( (array) $instance, array( 'title' => __( "We're in the battle for the net.", 'btfn' ), 'size' => BFTN_DEFAULT_SIZE, 'custom_image' => '' ) );
+        $instance = wp_parse_args( (array) $instance, array( 'title' => __( "We're in the battle for the net.", 'btfn' ), 'battle_image' => '3', 'custom_image' => '' ) );
         extract( $instance );
         ?>
             <p>
@@ -129,9 +139,12 @@ class BFTN_Widget extends WP_Widget {
               <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
             </p>
             <p>
-              <label for="<?php echo $this->get_field_id('size'); ?>"><?php _e('Size'); ?></label> 
-              <input class="widefat" id="<?php echo $this->get_field_id('size'); ?>" name="<?php echo $this->get_field_name('size'); ?>" type="text" value="<?php echo $size; ?>" />
-            </p>
+              <label for="<?php echo $this->get_field_id('battle_image'); ?>"><?php _e('Select Battle Image:'); ?></label>
+              <select id="<?php echo $this->get_field_id('battle_image'); ?>" name="<?php echo $this->get_field_name('battle_image'); ?>">
+				<option value="1" <?php selected( $instance['battle_image'], '1'); ?> >1</option>
+				<option value="2" <?php selected( $instance['battle_image'], '2'); ?> >2</option>
+				<option value="3" <?php selected( $instance['battle_image'], '3'); ?> >3</option>
+              </select>             
             <p>
               <label for="<?php echo $this->get_field_id('custom_image'); ?>"><?php _e('Use your own image, custom image url:'); ?></label> 
               <input class="widefat" id="<?php echo $this->get_field_id('custom_image'); ?>" name="<?php echo $this->get_field_name('custom_image'); ?>" type="text" value="<?php echo $custom_image; ?>" />
